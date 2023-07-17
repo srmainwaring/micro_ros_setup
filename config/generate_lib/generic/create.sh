9f6be5ee72a6b9228e63ed32ac1b66381bef6e70
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-07-17 08:07:50 +0100

    macOS: add geometry2 to packages to support tf2
    - Correct package name in exclusion list.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- config/generate_lib/generic/create.sh
+++ config/generate_lib/generic/create.sh
@@ -8,4 +8,20 @@ pushd $FW_TARGETDIR >/dev/null
     touch mcu_ws/uros/rcl/rcl_yaml_param_parser/COLCON_IGNORE
     touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
 
+    # ignore everything but messages in geometry2
+    touch mcu_ws/ros2/geometry2/examples_tf2_py/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/geometry2/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/test_tf2/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_bullet/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_eigen/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_eigen_kdl/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_geometry_msgs/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_kdl/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_py/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_ros/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_ros_py/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_sensor_msgs/COLCON_IGNORE
+    touch mcu_ws/ros2/geometry2/tf2_tools/COLCON_IGNORE
+
 popd >/dev/null
