a3fac2064febd7ada5bb8d4de7aa0dee196210d4
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-07-18 09:45:07 +0100

    macOS: build library with debug symbols and set verbose
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- config/generate_lib/generic/build.sh
+++ config/generate_lib/generic/build.sh
@@ -9,11 +9,11 @@ fi
 
 if [ $# -ge 2 ]; then
     COLCON_META=$2
-	echo "Using provided meta: $COLCON_META"
+    echo "Using provided meta: $COLCON_META"
 
 else
     COLCON_META=$FW_TARGETDIR/mcu_ws/colcon.meta
-	echo "Using default meta: $COLCON_META"
+    echo "Using default meta: $COLCON_META"
 fi
 
 
@@ -21,33 +21,56 @@ BUILD_DIR=$FW_TARGETDIR/build
 
 pushd $FW_TARGETDIR/mcu_ws >/dev/null
 
-	rm -rf build install log
-	
-   	colcon build \
-		--merge-install \
-		--packages-ignore-regex=.*_cpp \
-		--metas $COLCON_META \
-		--cmake-args \
-		"--no-warn-unused-cli" \
-		-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=OFF \
-		-DTHIRDPARTY=ON \
-		-DBUILD_SHARED_LIBS=OFF \
-		-DBUILD_TESTING=OFF \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
-		-DCMAKE_VERBOSE_MAKEFILE=ON; \
+    rm -rf build install log
 
-    mkdir -p $FW_TARGETDIR/libmicroros; cd $FW_TARGETDIR/libmicroros; \
-	for file in $(find $FW_TARGETDIR/mcu_ws/install/lib/ -name '*.a'); do \
-		folder=$(echo $file | sed -E "s/(.+)\/(.+).a/\2/"); \
-		mkdir -p $folder; cd $folder; ar x $file; \
-		for f in *; do \
-			mv $f ../$folder-$f; \
-		done; \
-		cd ..; rm -rf $folder; \
-	done ; \
-	ar rc libmicroros.a $(ls *.o *.obj 2> /dev/null); mkdir -p $BUILD_DIR; cp libmicroros.a $BUILD_DIR; ranlib $BUILD_DIR/libmicroros.a; \
-    cp -R $FW_TARGETDIR/mcu_ws/install/include $BUILD_DIR/; \
-	cd ..; rm -rf libmicroros;
+    colcon build \
+    --merge-install \
+    --packages-ignore-regex=.*_cpp \
+    --metas $COLCON_META \
+    --cmake-args \
+    "--no-warn-unused-cli" \
+    -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=OFF \
+    -DTHIRDPARTY=ON \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
+    -DCMAKE_VERBOSE_MAKEFILE=ON; \
 
+    # mkdir -p $FW_TARGETDIR/libmicroros; cd $FW_TARGETDIR/libmicroros; \
+    # for file in $(find $FW_TARGETDIR/mcu_ws/install/lib/ -name '*.a'); do \
+    #     folder=$(echo $file | sed -E "s/(.+)\/(.+).a/\2/"); \
+    #     mkdir -p $folder; cd $folder; ar x $file; \
+    #     for f in *; do \
+    #         mv $f ../$folder-$f; \
+    #     done; \
+    #     cd ..; rm -rf $folder; \
+    # done ; \
+    # ar rc libmicroros.a $(ls *.o *.obj 2> /dev/null); mkdir -p $BUILD_DIR; cp libmicroros.a $BUILD_DIR; ranlib $BUILD_DIR/libmicroros.a; \
+    # cp -R $FW_TARGETDIR/mcu_ws/install/include $BUILD_DIR/; \
+    # cd ..; rm -rf libmicroros;
+
+    mkdir -p $FW_TARGETDIR/libmicroros
+    cd $FW_TARGETDIR/libmicroros
+    for file in $(find $FW_TARGETDIR/mcu_ws/install/lib -name '*.a'); do
+        echo file=$file
+        folder=$(echo $file | sed -E "s/(.+)\/(.+).a/\2/");
+        echo folder=$folder
+        mkdir -p $folder
+        cd $folder
+        ar x $file
+        for f in *.o; do
+            mv "$f" ../$folder-$f;
+        done ;
+        cd ..
+        rm -rf $folder;
+    done ;
+    ar rc libmicroros.a $(ls *.o *.obj 2> /dev/null)
+    rm -f *.o *.obj
+    mkdir -p $BUILD_DIR
+    cp libmicroros.a $BUILD_DIR
+    ranlib $BUILD_DIR/libmicroros.a
+    cp -R $FW_TARGETDIR/mcu_ws/install/include $BUILD_DIR
+    cd ..
+    rm -rf libmicroros
 popd >/dev/null
